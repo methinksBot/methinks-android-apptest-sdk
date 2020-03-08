77fcb55db18cf3f4aa55ed77c8e6ef84ccda6333
SETH <seth@methinks.io>
2020-03-08 15:58:33 -0700

    login error dialog

---
+++ app/src/main/java/io/methinks/android/apptest/ErrorDialogFragment.java
@@ -0,0 +1,46 @@
+package io.methinks.android.apptest;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.Toast;
+
+public class ErrorDialogFragment extends DialogFragment {
+
+    private String errorString;
+
+    @SuppressLint("ValidFragment")
+    public ErrorDialogFragment(String errorCode) {
+        this();
+        switch (errorCode) {
+            case "removedFromCampaign":
+                this.errorString = "You are no longer a part of this project.";
+                break;
+            case "cantSeeProject":
+                this.errorString = "The project has ended.";
+                break;
+            case "invalidUserCode":
+                this.errorString = "Invalid code.";
+                break;
+            default:
+                break;
+        }
+    }
+
+    public ErrorDialogFragment() {}
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(errorString)
+                .setPositiveButton(R.string.common_text_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) { }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
