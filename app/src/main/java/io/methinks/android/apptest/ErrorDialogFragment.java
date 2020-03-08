9ae897d4a4f89d22fbb5dfd4c7228fd5aaa18208
SETH <seth@methinks.io>
2020-03-08 16:29:08 -0700

    theme change

--- app/src/main/java/io/methinks/android/apptest/ErrorDialogFragment.java
+++ app/src/main/java/io/methinks/android/apptest/ErrorDialogFragment.java
@@ -35,7 +35,7 @@ public class ErrorDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.AccountAccessActivityTheme);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyDialogTheme);
         builder.setMessage(errorString)
                 .setPositiveButton(R.string.common_text_ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) { }
