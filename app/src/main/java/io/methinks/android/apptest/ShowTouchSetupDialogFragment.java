ba1f1a34028601a2d5aeda21142c12261914c848
SETH <seth@methinks.io>
2020-03-09 16:22:35 -0700

    show touches dialog layout

--- app/src/main/java/io/methinks/android/apptest/ShowTouchSetupDialogFragment.java
+++ app/src/main/java/io/methinks/android/apptest/ShowTouchSetupDialogFragment.java
@@ -6,10 +6,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+
+import java.util.zip.Inflater;
 
 public class ShowTouchSetupDialogFragment extends DialogFragment {
 
     private String errorString;
+
 
     public ShowTouchSetupDialogFragment() {}
 
@@ -17,17 +21,19 @@ public class ShowTouchSetupDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyDialogTheme);
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
         builder.setTitle(R.string.patcher_req_show_touch)
-                .setMessage(R.string.patcher_show_touch_setup)
-                .setMessage(R.string.patcher_show_touch_setup_option_first)
-                .setMessage(R.string.patcher_show_touch_setup_option_second)
+                .setView(inflater.inflate(R.layout.sdk_show_touches_dialog, null))
                 .setPositiveButton(R.string.patcher_show_touch_option_first, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        /** Enable show touches on user's own */
                         startActivityForResult(new Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS), Global.REQUEST_SHOW_TOUCHES);
                     }
                 })
-                .setPositiveButton(R.string.patcher_show_touch_option_second, new DialogInterface.OnClickListener() {
+                .setNegativeButton(R.string.patcher_show_touch_option_second, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        /** Enable show touches by methinks_show_touches Extension app */
 
                     }
                 })
