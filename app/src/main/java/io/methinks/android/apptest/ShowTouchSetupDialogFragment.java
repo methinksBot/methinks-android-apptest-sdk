c988fa4552bd9c1f3118b5eb4dd64fddc5f6e616
SETH <seth@methinks.io>
2020-03-10 18:52:44 -0700

    set fileprovider

--- app/src/main/java/io/methinks/android/apptest/ShowTouchSetupDialogFragment.java
+++ app/src/main/java/io/methinks/android/apptest/ShowTouchSetupDialogFragment.java
@@ -169,10 +169,12 @@ public class ShowTouchSetupDialogFragment extends DialogFragment {
                 if (id == downloadID) {
                     Uri contentUri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".provider", new File(activity.getExternalFilesDir(null), "methinks_touchsupports.apk"));
                     Log.w("[URI]: "+ contentUri.toString());
+                    context.grantUriPermission(context.getPackageName(), contentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
                     Intent openFileIntent = new Intent(Intent.ACTION_VIEW);
                     openFileIntent.addCategory("android.intent.category.DEFAULT");
                     openFileIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    openFileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     openFileIntent.setDataAndType(contentUri, "application/vnd.android.package-archive");
                     activity.startActivityForResult(openFileIntent, Global.EXTENTION_INSTALL_DONE);
                     context.unregisterReceiver(this);
