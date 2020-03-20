545d520e78ed5dc1782eaad6d3e529cfde71354a
SETH <seth@methinks.io>
2020-03-19 23:57:52 -0700

    block screenshot on every activity

--- app/src/main/java/io/methinks/android/apptest/ApplicationTracker.java
+++ app/src/main/java/io/methinks/android/apptest/ApplicationTracker.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -44,6 +45,7 @@ public class ApplicationTracker {
     public void addManually(Activity activity, final ActivityReadyCallback callback){
         Log.d("Adding activity to Activity State Tracker manually because Activity is passed onResume() already. ");
         activityStack.add(activity);
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         activityLifes.put(activity.getClass().getSimpleName(), "resume");
 
         isBackground = false;
@@ -66,6 +68,7 @@ public class ApplicationTracker {
             @Override
             public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
                 Log.d("onActivityCreated() : " + activity.getClass().getSimpleName());
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
                 activityLifes.put(activity.getClass().getSimpleName(), "create");
                 if(Global.hoverIntent == null){
                     Global.hoverIntent = new Intent(activity, HService.class);
