e202d7da4e9d9e27742290c9b76edac45621bae8
SETH <seth@methinks.io>
2020-03-20 20:00:25 -0700

    blocking screen capture for nexon

--- app/src/main/java/io/methinks/android/apptest/LoginActivity.java
+++ app/src/main/java/io/methinks/android/apptest/LoginActivity.java
@@ -41,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
             logoImageView.setImageResource(R.drawable.img_logo_methinks);
         }
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
 
         EditText testUserCodeEditText = (EditText)findViewById(R.id.testUserCode);
         testUserCodeEditText.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#4D77DD")));
